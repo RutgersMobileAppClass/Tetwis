2b51ccf9cd070bb1dab21e891a6578c1c4d48e83
Jonathan Zelaya <zelaya0805@gmail.com>
2016-11-30 00:50:47 -0500

    basic game working, needs more work. needs to change parameters to have a better feel and look.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SpawnNextTetromino ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SpawnNextTetromino(){
+		Object o = Resources.Load(GetRandomTetromino(),typeof(GameObject));
+		Vector2 location = new Vector2 (0f, 18f);
+		GameObject next = (GameObject)Instantiate (o, location, Quaternion.identity);
+	}
+
+	private string GetRandomTetromino(){
+		int randomNumber = Random.Range (1, 8);
+
+		string randomTetrominoName = "Tetromino_T";
+
+		switch (randomNumber) {
+		case 1:
+			randomTetrominoName = "Prefabs/Tetromino_J";
+			break;
+		case 2:
+			randomTetrominoName = "Prefabs/Tetromino_L";
+			break;
+		case 3:
+			randomTetrominoName = "Prefabs/Tetromino_Long";
+			break;
+		case 4:
+			randomTetrominoName = "Prefabs/Tetromino_S";
+			break;
+		case 5:
+			randomTetrominoName = "Prefabs/Tetromino_Square";
+			break;
+		case 6:
+			randomTetrominoName = "Prefabs/Tetromino_T";
+			break;
+		case 7:
+			randomTetrominoName = "Prefabs/Tetromino_Z";
+			break;
+		}
+		return randomTetrominoName;
+		//return "Prefabs/Tetromino_Square";
+	}
+}
---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tetromino : MonoBehaviour{
+	private bool enabled = true;
+	private Rigidbody2D rb2d;
+	private float mass;
+	private GameObject light;
+
+	// Use this for initialization
+	void Start (){
+		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.velocity = new Vector2 (0.0f, -5.0f);
+		mass = rb2d.mass;
+		//rb2d.inertia = 1.0f;
+
+	}
+
+	// Update is called once per frame
+	void Update (){
+		checkUserInput ();
+		if (rb2d.velocity.y > -5.0f) {
+			rb2d.velocity = rb2d.velocity - new Vector2(0.0f,0.5f);
+		}
+		if (rb2d.position.y < -12) {
+			if (enabled) {
+				FindObjectOfType<Game> ().SpawnNextTetromino();
+			}
+			Destroy (gameObject);
+		}
+			
+	}
+
+	void checkUserInput (){
+		if (!enabled) {
+			return;
+		}
+			
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			transform.position += new Vector3 (1.0f, 0, 0);
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			transform.position += new Vector3 (-1.0f, 0, 0);
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			transform.Rotate (0, 0, 90);
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			rb2d.velocity = new Vector2 (0.0f,-15.0f);
+		} else if (Input.GetKeyUp(KeyCode.DownArrow)){
+			rb2d.velocity = new Vector2(0.0f,-5.0f);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (enabled) {
+			enabled = false;
+			FindObjectOfType<Game> ().SpawnNextTetromino();
+			rb2d.mass = rb2d.mass * 1000000;
+		}
+	}
+
+	void OnTriggerStay2D(){
+		//cam = (Camera) GameObject.FindGameObjectWithTag ("MainCamera");
+		//cam.transform.position = cam.transform.position + new Vector3 (0.0f, 5.0f);
+	}
+
+	void OnTriggerExit2D(){
+		
+	}
+		
+}
