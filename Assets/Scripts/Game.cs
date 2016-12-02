6147b9ccacf3325f0e8f5277f82d23961fab61ec
Snowrries <allentung5@gmail.com>
2016-12-02 10:19:44 -0500

    Textures and Scene switching

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -15,7 +15,7 @@ public class Game : MonoBehaviour {
 
 	public void SpawnNextTetromino(){
 		Object o = Resources.Load(GetRandomTetromino(),typeof(GameObject));
-		Vector2 location = new Vector2 (0f, 18f);
+		Vector2 location = new Vector2 (0f, 20f);
 		GameObject next = (GameObject)Instantiate (o, location, Quaternion.identity);
 	}
 
@@ -26,26 +26,33 @@ public class Game : MonoBehaviour {
 
 		switch (randomNumber) {
 		case 1:
-			randomTetrominoName = "Prefabs/Tetromino_J";
-			break;
+                //randomTetrominoName = "Prefabs/Tetromino_J";
+                randomTetrominoName = "Prefabs/Leaves_J";
+                break;
 		case 2:
-			randomTetrominoName = "Prefabs/Tetromino_L";
+                //randomTetrominoName = "Prefabs/Tetromino_L";
+                randomTetrominoName = "Prefabs/Purple_L";
 			break;
 		case 3:
-			randomTetrominoName = "Prefabs/Tetromino_Long";
-			break;
+                //randomTetrominoName = "Prefabs/Tetromino_Long";
+                randomTetrominoName = "Prefabs/Brick_Long";
+                break;
 		case 4:
-			randomTetrominoName = "Prefabs/Tetromino_S";
-			break;
+                //randomTetrominoName = "Prefabs/Tetromino_S";
+                randomTetrominoName = "Prefabs/Orange_S";
+                break;
 		case 5:
-			randomTetrominoName = "Prefabs/Tetromino_Square";
-			break;
+                //randomTetrominoName = "Prefabs/Tetromino_Square";
+                randomTetrominoName = "Prefabs/Ice_Square";
+                break;
 		case 6:
-			randomTetrominoName = "Prefabs/Tetromino_T";
-			break;
+                //randomTetrominoName = "Prefabs/Tetromino_T";
+                randomTetrominoName = "Prefabs/Concrete_T";
+                break;
 		case 7:
-			randomTetrominoName = "Prefabs/Tetromino_Z";
-			break;
+                //randomTetrominoName = "Prefabs/Tetromino_Z";
+                randomTetrominoName = "Prefabs/Wood_Z";
+                break;
 		}
 		return randomTetrominoName;
 		//return "Prefabs/Tetromino_Square";
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -3,18 +3,25 @@ using System.Collections;
 
 public class Tetromino : MonoBehaviour{
 	private bool enabled = true;
-	private Rigidbody2D rb2d;
+    private bool onTheFly = false;
+    private bool inGrace = false;
+    private Rigidbody2D rb2d;
 	private float mass;
 	private GameObject light;
+    private GameObject temi;
+    private Vector2 forceLeft;
+    private Vector2 forceRight;
 
-	// Use this for initialization
-	void Start (){
+    // Use this for initialization
+    void Start (){
 		rb2d = GetComponent<Rigidbody2D> ();
 		rb2d.velocity = new Vector2 (0.0f, -5.0f);
 		mass = rb2d.mass;
-		//rb2d.inertia = 1.0f;
+        forceLeft = new Vector2(-1, 0);
+        forceRight = new Vector2(1, 0);
+        //rb2d.inertia = 1.0f;
 
-	}
+    }
 
 	// Update is called once per frame
 	void Update (){
@@ -35,29 +42,45 @@ public class Tetromino : MonoBehaviour{
 		if (!enabled) {
 			return;
 		}
-			
-		if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			transform.position += new Vector3 (1.0f, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            if (onTheFly)
+                rb2d.AddForce(forceRight, ForceMode2D.Impulse);
+            else
+                transform.position += new Vector3 (1.0f, 0, 0);
 		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			transform.position += new Vector3 (-1.0f, 0, 0);
+            if (onTheFly)
+                rb2d.AddForce(forceLeft, ForceMode2D.Impulse);
+            else
+                transform.position += new Vector3 (-1.0f, 0, 0);
 		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			transform.Rotate (0, 0, 90);
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			rb2d.velocity = new Vector2 (0.0f,-15.0f);
-		} else if (Input.GetKeyUp(KeyCode.DownArrow)){
+        } else if (Input.GetKeyUp(KeyCode.DownArrow)){
 			rb2d.velocity = new Vector2(0.0f,-5.0f);
 		}
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
-		if (enabled) {
-			enabled = false;
-			FindObjectOfType<Game> ().SpawnNextTetromino();
-			rb2d.mass = rb2d.mass * 1000000;
+		if (enabled && !inGrace) {
+            StartCoroutine(grace());
 		}
 	}
+    IEnumerator grace()
+    {
+        inGrace = true;
+        //Wait one second after the first collision and allow user to make on-the-fly corrections
+        //Then cut off the user actions and make a new block
+        yield return new WaitForSecondsRealtime(0.5f);
+        onTheFly = true;
+        enabled = false;
+        FindObjectOfType<Game>().SpawnNextTetromino();
+        rb2d.mass = rb2d.mass * 1000000;
+        inGrace = false;
+    }
 
-	void OnTriggerStay2D(){
+    void OnTriggerStay2D(){
 		//cam = (Camera) GameObject.FindGameObjectWithTag ("MainCamera");
 		//cam.transform.position = cam.transform.position + new Vector3 (0.0f, 5.0f);
 	}
